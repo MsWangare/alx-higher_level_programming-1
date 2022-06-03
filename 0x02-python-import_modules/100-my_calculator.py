ead4ce22bb8b1e053a011c737f9ae4e804b322f6
Lehlohonolo Lehoko <hlonilehoko29@yahoo.com>
2022-06-03 07:23:39 -0700

    6. Build my own calculator! [2]

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -18,4 +18,5 @@ if __name__ == "__main__":
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
     else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
