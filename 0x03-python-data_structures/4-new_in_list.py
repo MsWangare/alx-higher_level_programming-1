61992494c01f87134212b8819c8804be9e41a8a0
Lehlohonolo Lehoko <hlonilehoko29@yahoo.com>
2022-06-05 11:43:58 -0700

    4. Replace in a copy [1]

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if (my_list):
-        my_list2 = my_list
-        if idx > -1 and idx < len(my_list2):
-            my_list2[idx] = element
-            return(my_list2)
+def new_in_list(my_list, idx, element):
+    my_list2 = my_list.copy()
+    if idx > -1 and idx < len(my_list2):
+        my_list2[idx] = element
+        return(my_list2)
     else:
+        my_list2 = my_list
         return(my_list2)
