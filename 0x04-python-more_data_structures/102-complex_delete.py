21aa29428b6e81d3bcb07327a4ad78ac476b279d
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-06-11 03:13:42 +0200

    15. Delete by value [2]

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-def complex_delete(my_dict, value):
-    keys_to_del = []
-    for key in my_dict:
-        if my_dict[key] == value:
-            keys_to_del.append(key)
-        for key in keys_to_del:
-                del my_dict[key]
-        return(my_dict)
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if value is None:
+        return a_dictionary
+    if a_dictionary is None or len(list(a_dictionary.keys())) == 0:
+        return None
+    if value not in list(a_dictionary.values()):
+        return a_dictionary
+    a_list = list(a_dictionary.items())
+    for i in a_list:
+        if value == i[1]:
+            del a_dictionary[i[0]]
+            continue
+    return a_dictionary
